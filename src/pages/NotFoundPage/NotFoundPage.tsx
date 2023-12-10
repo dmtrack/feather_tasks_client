f2c850434073f0ed54b0e7c236824769aab3c838
dmtrack <rightrack888@gmail.com>
2023-12-10 20:29:08 +0500

    feat: sign in pages

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,26 @@
+import React, { memo } from 'react';
+
+import Button from 'components/Button/Button';
+
+import { MainWrapper, Title } from 'styles/styles';
+
+import { NotFoundText, NotFoundLink, NotFoundIcon } from './NotFoundPage.style';
+
+function NotFoundPage() {
+    return (
+        <MainWrapper>
+            <NotFoundIcon />
+            <Title>Something went wrong!</Title>
+            <NotFoundText>
+                Unfortunately, this page does not exist.
+            </NotFoundText>
+            <NotFoundLink to="/">
+                <Button type="button" width="210px">
+                    Back home
+                </Button>
+            </NotFoundLink>
+        </MainWrapper>
+    );
+}
+
+export default memo(NotFoundPage);
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,10 +1,79 @@
 import React, { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { backButtonAnimation } from 'utils/animations';
+
+import useSignUpUser from 'hooks/useSignUpUser';
+
+import AuthForm from 'components/AuthForm/AuthForm';
+import Button from 'components/Button/Button';
+import Loader from 'components/Loader/Loader';
+import PopupNotification from 'components/PopupNotification/PopupNotification';
+
+import {
+    FormDescriptionWrapper,
+    FormDescriptionText,
+    FormWrapper,
+    MainWrapper,
+    StyledLink,
+    Title,
+    FormHeader,
+    StyledPrevIcon,
+    LinkWrapper,
+} from 'styles/styles';
+import defaultTheme from 'styles/theme';
+import { setErrorNotificationPopupOpen } from 'redux/slices/userSlice';
+
 function SignUpPage() {
     const navigate = useNavigate();
+    const {
+        isErrorNotificationPopupOpen,
+        isLoadingAuth,
+        signUpErrorMessage,
+        onSubmit,
+    } = useSignUpUser();
 
-    return <div>SignInPage</div>;
+    return (
+        <MainWrapper>
+            <FormWrapper>
+                <FormHeader>
+                    <Button
+                        type="button"
+                        width="30px"
+                        animation={backButtonAnimation}
+                        backgroundColor={defaultTheme.colors.transparent}
+                        disabled={isLoadingAuth}
+                        callback={() => navigate(-1)}
+                    >
+                        <StyledPrevIcon $isDisabled={isLoadingAuth} />
+                    </Button>
+                </FormHeader>
+                <Title>Sign Up</Title>
+                <AuthForm
+                    keyPrefix="signUp"
+                    onSubmit={onSubmit}
+                    isLoadingAuth={isLoadingAuth}
+                />
+                <FormDescriptionWrapper>
+                    <FormDescriptionText>or</FormDescriptionText>
+                </FormDescriptionWrapper>
+                <LinkWrapper>
+                    <StyledLink to="/signin">
+                        Already have an account? Sign In
+                    </StyledLink>
+                </LinkWrapper>
+            </FormWrapper>
+            {isLoadingAuth && <Loader />}
+            {signUpErrorMessage && (
+                <PopupNotification
+                    isPopupShown={isErrorNotificationPopupOpen}
+                    setPopupShown={setErrorNotificationPopupOpen}
+                    text="This login already exists"
+                    backgroundColor={defaultTheme.colors.pink}
+                />
+            )}
+        </MainWrapper>
+    );
 }
 
 export default memo(SignUpPage);
